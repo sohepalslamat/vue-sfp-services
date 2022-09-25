4b54ad151d46c462f9bb388d8915dbf57aa52da9
Vuango Simply Learn <sohep.alslamat91@gmail.com>
2022-09-25 21:18:01 +0300

    add description to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,12 @@ git checkout main
 
 # deploy production branch to github
 gh-pages -b production -d dist
+
+
+# production branch for demo:
+#  - any changes in demo will be in thi branch
+#  - no push no pull from origin in this branch
+#  - 
+# main branch for package in npm 
+#  - any changes will be for package
+#  - build in this branch after that npm publish to deploy it to npm
